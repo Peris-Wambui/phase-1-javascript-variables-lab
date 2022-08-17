6d9514be3ae676917a3bdb8afb2076f9e8992ff2
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-08-16 22:42:18 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+// let companyName = 'Scuber';
+
+// if (companyName === 'Scuber') {
+//     let companyName ="companyName";
+//     console .log(companyName);
+// }
+// console .log(companyName)
+const companyName ='Scuber';
+
+let mostProfitableNeighborhood="Chelsea";
+let companyCeo="Susan Smith";
